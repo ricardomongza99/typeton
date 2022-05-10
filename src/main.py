f6092b2e2b2ed664c9e404a982f2b2a778c5ed53
Ricardo Montemayor <ricardomongza@gmail.com>
2022-05-09 23:02:30 -0500

    Fix relative FILENAME path issue

--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
 from semantic import run_tests, Cube
 from parser import parser
+import os
+
 
 FILENAME = 'sheep.ty'
 
 
 def main():
-    file = open('../programs/' + FILENAME)
+    # Get relative path
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, '../programs/' + FILENAME)
+
+    file = open(filename)
     data = file.read()
     file.close()
 
