c3bdcc34d512b4a4a14e77b89ddfa2c004573468
Ricardo Montemayor <ricardomongza@gmail.com>
2022-05-12 10:36:24 -0500

    Simplify add and set type for DirFunc

--- src/dir_func.py
+++ src/dir_func.py
@@ -14,18 +14,26 @@ class DirFunc:
     def __init__(self):
         self.functions = {}
         self.current_id = 'global'
-        self.add('global', 'Void')
+        self.add('global')
 
-    def add(self, id_, type_):
+    @property
+    def current_func(self):
+        return self.functions[self.current_id]
+
+    def add(self, id_):
         """ Add Function to `functions` dictionary if not existent """
         if self.functions.get(id_) is None:
             self.current_id = id_
-            self.functions[id_] = Function(type_)
+            self.functions[id_] = Function('Void')
 
     def set_type(self, type_):
-        self.functions[self.current_id].type_ = type_
+        """ Sets current function type """
+        self.current_func.type_ = type_
 
-    def display(self):
+    def add_variable(self, id_):
+        self.current_func.vars_table.add(id_)
+
+    def display(self, show_var_tables=False):
         print("-" * 20)
         print("DIRECTORY FUNCTIONS")
         print("-" * 20)
@@ -34,3 +42,9 @@ class DirFunc:
         for id_, function in self.functions.items():
             print('{:10} {:10}'.format(id_, function.type_))
         print("-" * 20)
+
+        if show_var_tables:
+            for id_, function in self.functions.items():
+                print("\n\n")
+                function.vars_table.display(id_)
+
