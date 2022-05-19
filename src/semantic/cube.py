3eabd79ee67c7246fa09ce14d158cdce1aadbdaa
Ricardo Montemayor <ricardomongza@gmail.com>
2022-05-18 20:39:04 -0500

    Refactor Cube

--- src/semantic/cube.py
+++ src/semantic/cube.py
@@ -1,114 +1,127 @@
 
 class Cube:
     def __init__(self):
-        self.__cube = {}
-        self.load_types()
+        self._cube = {}
+        # Fill cube
+        self._add(arithmetic_symbols, arithmetic_combinations)
+        self._add(comparison_symbols, comparison_combinations)
+        self._add(assign_symbol, assign_combinations)
+        self._add(boolean_symbols, boolean_combinations)
 
-    def load_types(self):
-        init_cube(self)
+    def _add(self, operators, combinations):
+        """
+        Adds combination key to dictionary with its result type
 
-    def insert(self, operators, compatible_types):
-        for op in operators:
-            for options in compatible_types:
-                left, right, result = options
-                self.__cube[f'{op}:{left}:{right}'] = result
-                self.__cube[f'{op}:{right}:{left}'] = result
+        :param operators: list of operators where the following combinations apply
+        :param combinations: list of combinations [left, right, result]
+        """
+        for operator in operators:
+            for left_type, right_type, result_type in combinations:
+                self._cube[f'{operator}:{left_type}:{right_type}'] = result_type
+                self._cube[f'{operator}:{right_type}:{left_type}'] = result_type
 
     def check(self, operator, left, right):
-        value = self.__cube[f'{operator}:{left}:{right}']
+        """
+        Checks and returns expected type for given parameters.
 
-        if value is not None:
-            return True, value
+        :param operator: the operator to apply
+        :param left: the left operand type
+        :param right: the right operand type
+        :return: the resulting type if exists, None otherwise
+       """
+        result = self._cube[f'{operator}:{left}:{right}']
 
-        return False, None
+        return result
 
 
-def init_cube(cube):
-    cube.insert(["-", "+", "*", "/"], [
-        ['int', 'float', 'float'],
-        ['int', 'int', 'int'],
-        ['float', 'float', 'float']
-    ])
+arithmetic_symbols = ['+', '-', '*', '/']
+arithmetic_combinations = [
+    ['Int', 'Float', 'Float'],
+    ['Int', 'Int', 'Int'],
+    ['Float', 'Float', 'Float']
+]
 
-    cube.insert([">", ">=", "<", "<=", "==", "!="], [
-        ['int', 'float', 'bool'],
-        ['int', 'int', 'bool'],
-        ['float', 'float', 'bool'],
-        ['bool', 'bool', 'bool'],
-    ])
+comparison_symbols = ['==', '!=', '<', '<=', '>', '>=']
+comparison_combinations = [
+    ['Int', 'Float', 'Bool'],
+    ['Int', 'Int', 'Bool'],
+    ['Float', 'Float', 'Bool'],
+    ['Bool', 'Bool', 'Bool'],
+]
 
-    cube.insert(["="], [
-        ['int', 'float', 'float'],
-        ['int', 'int', 'int'],
-        ['float', 'float', 'float'],
-        ['bool', 'bool', 'bool'],
-    ])
+assign_symbol = ['=']
+assign_combinations = [
+    ['Int', 'Float', 'Float'],
+    ['Int', 'Int', 'Int'],
+    ['Float', 'Float', 'Float'],
+    ['Bool', 'Bool', 'Bool'],
+]
 
-    cube.insert(["&&", "||"], [
-        ['bool', 'bool', 'bool'],
-    ])
+boolean_symbols = ['&&', '||']
+boolean_combinations = [
+    ['Bool', 'Bool', 'Bool']
+]
 
 
 def run_tests(cube):
     tests = [
-        ["+", "int", "int", "int"],
-        ["+", "float", "float", "float"],
-        ["+", "float", "int", "float"],
-        ["-", "int", "int", "int"],
-        ["-", "float", "float", "float"],
-        ["-", "float", "int", "float"],
-        ["*", "int", "int", "int"],
-        ["*", "float", "float", "float"],
-        ["*", "float", "int", "float"],
-        ["/", "int", "int", "int"],
-        ["/", "float", "float", "float"],
-        ["/", "float", "int", "float"],
+        ["+", "Int", "Int", "Int"],
+        ["+", "Float", "Float", "Float"],
+        ["+", "Float", "Int", "Float"],
+        ["-", "Int", "Int", "Int"],
+        ["-", "Float", "Float", "Float"],
+        ["-", "Float", "Int", "Float"],
+        ["*", "Int", "Int", "Int"],
+        ["*", "Float", "Float", "Float"],
+        ["*", "Float", "Int", "Float"],
+        ["/", "Int", "Int", "Int"],
+        ["/", "Float", "Float", "Float"],
+        ["/", "Float", "Int", "Float"],
 
-        ["&&", "bool", "bool", "bool"],
-        ["||", "bool", "bool", "bool"],
+        ["&&", "Bool", "Bool", "Bool"],
+        ["||", "Bool", "Bool", "Bool"],
 
-        ["=", 'int', 'float', 'float'],
-        ["=", 'int', 'int', 'int'],
-        ["=", 'float', 'float', 'float'],
-        ["=", 'bool', 'bool', 'bool'],
+        ["=", 'Int', 'Float', 'Float'],
+        ["=", 'Int', 'Int', 'Int'],
+        ["=", 'Float', 'Float', 'Float'],
+        ["=", 'Bool', 'Bool', 'Bool'],
 
-        [">", 'int', 'float', 'bool'],
-        [">", 'int', 'int', 'bool'],
-        [">", 'float', 'float', 'bool'],
-        [">", 'bool', 'bool', 'bool'],
+        [">", 'Int', 'Float', 'Bool'],
+        [">", 'Int', 'Int', 'Bool'],
+        [">", 'Float', 'Float', 'Bool'],
+        [">", 'Bool', 'Bool', 'Bool'],
 
-        ["<", 'int', 'float', 'bool'],
-        ["<", 'int', 'int', 'bool'],
-        ["<", 'float', 'float', 'bool'],
-        ["<", 'bool', 'bool', 'bool'],
+        ["<", 'Int', 'Float', 'Bool'],
+        ["<", 'Int', 'Int', 'Bool'],
+        ["<", 'Float', 'Float', 'Bool'],
+        ["<", 'Bool', 'Bool', 'Bool'],
 
-        ["<=", 'int', 'float', 'bool'],
-        ["<=", 'int', 'int', 'bool'],
-        ["<=", 'float', 'float', 'bool'],
-        ["<=", 'bool', 'bool', 'bool'],
+        ["<=", 'Int', 'Float', 'Bool'],
+        ["<=", 'Int', 'Int', 'Bool'],
+        ["<=", 'Float', 'Float', 'Bool'],
+        ["<=", 'Bool', 'Bool', 'Bool'],
 
-        [">=", 'int', 'float', 'bool'],
-        [">=", 'int', 'int', 'bool'],
-        [">=", 'float', 'float', 'bool'],
-        [">=", 'bool', 'bool', 'bool'],
+        [">=", 'Int', 'Float', 'Bool'],
+        [">=", 'Int', 'Int', 'Bool'],
+        [">=", 'Float', 'Float', 'Bool'],
+        [">=", 'Bool', 'Bool', 'Bool'],
 
-        ["==", 'int', 'float', 'bool'],
-        ["==", 'int', 'int', 'bool'],
-        ["==", 'float', 'float', 'bool'],
-        ["==", 'bool', 'bool', 'bool'],
+        ["==", 'Int', 'Float', 'Bool'],
+        ["==", 'Int', 'Int', 'Bool'],
+        ["==", 'Float', 'Float', 'Bool'],
+        ["==", 'Bool', 'Bool', 'Bool'],
 
-        ["!=", 'int', 'float', 'bool'],
-        ["!=", 'int', 'int', 'bool'],
-        ["!=", 'float', 'float', 'bool'],
-        ["!=", 'bool', 'bool', 'bool'],
-
+        ["!=", 'Int', 'Float', 'Bool'],
+        ["!=", 'Int', 'Int', 'Bool'],
+        ["!=", 'Float', 'Float', 'Bool'],
+        ["!=", 'Bool', 'Bool', 'Bool'],
     ]
 
     errors = 0
     for t in tests:
 
-        is_valid, value = cube.check(t[0], t[1], t[2])
-        if not is_valid:
+        value = cube.check(t[0], t[1], t[2])
+        if value is None:
             errors += 1
             print("error on test:", t, "expected: ", t[3], ", actual: ", value)
     if errors == 0:
