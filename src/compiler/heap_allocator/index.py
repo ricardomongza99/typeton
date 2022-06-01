adca6747fb44e7ccda0575130b420b5e0647a1f2
Francisco Sainz <pacoswilliams@gmail.com>
2022-05-31 23:30:19 -0500

    Implemented Compilation Heap Allocator
    Allocates resources that will be used for storing objects and arrays

--- src/compiler/heap_allocator/index.py
+++ src/compiler/heap_allocator/index.py
@@ -1,83 +1,84 @@
-import heapq
+from functools import cmp_to_key
 
 
-class HeapRange:
+class FreeRange:
+    """Represents a continuous amount of available memory in the heap"""
+
     def __init__(self, start, end):
         self.start = start
         self.end = end
 
-    def __lt__(self, other):
-        return self.size < other.val
-
     @property
     def size(self):
-        return self.end - self.start
+        return (self.end - self.start) + 1
+
+    def __str__(self):
+        return f'({self.start}, {self.end})'
+
+    def __lt__(self, other):
+        return self.size > other.size
 
 
-class MaxRangeHeap:
-    """Get the largest slot of available space in the memory heap"""
+def compare(a, b):
+    return a.start - b.start or a.end - b.end
 
-    def __init__(self, size):
-        self.heap = []
-        heapq.heapify(self.heap)
-        self.push_range(HeapRange(0, size - 1))
-
-    def pop(self):
-        """get value from tuple"""
-        return heapq.heappop(self.heap)[1]
-
-    @property
-    def largest(self):
-        """get value from tuple"""
-        return self.heap[0][1]
-
-    def push_range(self, heap_range: HeapRange):
-        """* -1 to make it a max heap (max at the top)"""
-        heapq.heappush(self.heap, (heap_range.size * -1, heap_range))
-
-
-# 0 4
-# 0 10
-
-# 0 - 4 (5), (5 - 9) 5
 
 class HeapAllocator:
     def __init__(self, size):
         self.size = size
-        self.heap = MaxRangeHeap(size)
-        self.used = 0
-        self.range_end = {}
-
-    def available(self):
-        return self.size - self.used
+        self.ranges = [FreeRange(0, size - 1)]
+        self.end_map = {}
 
     def allocate_reference(self, size):
-        if self.available() == 0:
-            return None
-        largest_free_block = self.heap.largest.size
+        """Take the biggest range possible, break it up if needed"""
+        if len(self.ranges) == 0:
+            return print('out of mem')
+        largest_free_block = self.ranges[0].size
         if size > largest_free_block:
             print("out of memory")
             return
 
-        free_block = self.heap.pop()
+        free_block = self.ranges.pop()
         if size == largest_free_block:
+            print("all memory used")
             return free_block.start
 
         reference = free_block.start
         new_start = free_block.start + size
         free_block.start = new_start
 
-        self.range_end[reference] = new_start - 1
-
-        self.heap.push_range(free_block)
-        self.used += size
+        self.ranges.append(free_block)
+        self.end_map[reference] = new_start - 1
+        # maintain references for instant access and range optimization
         return reference
 
     def display(self):
-        for size, range in self.heap.heap:
-            print("Free Ranges")
+        print('----------------------------------------')
+        for range_ in self.ranges:
+            print(range_)
+
+        if len(self.ranges) == 0:
+            print("no spaces left")
+
+        print('----------------------------------------')
+
+    def _merge_intervals(self):
+        """To prevent fragmentation, always merge continuous intervals if possible"""
+        self.ranges.sort(key=cmp_to_key(compare))
+
+        result = []
+
+        last = None
+        for range_ in self.ranges:
+            if not last or range_.start > last.end + 1:
+                last = range_
+                result.append(last)
+            elif range_.end > last.end:
+                last.end = range_.end
+
+        self.ranges = result
 
     def free_reference(self, reference):
-        """Adds range to be able to usable again"""
-        self.heap.push_range(HeapRange(reference, self.range_end[reference]))
-        self.used -= (self.range_end[reference] - reference) + 1
+        """Adds range to be able to use it again"""
+        self.ranges.append(FreeRange(reference, self.end_map[reference]))
+        self._merge_intervals()
