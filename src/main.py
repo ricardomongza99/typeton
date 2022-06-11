56b49879776bdeac862c3e74e9c67bad229b8e70
Ricardo Montemayor <ricardomongza@gmail.com>
2022-06-11 14:41:37 -0500

    Add program and debug arguments

--- src/main.py
+++ src/main.py
@@ -1,20 +1,42 @@
 from src.compiler import Compiler
 from src.virtual_machine import VirtualMachine
 import os
+import sys
 from src.config.definitions import PROGRAMS_DIR
 
-FILENAME = 'book.ty'
-
+DEFAULT_PROGRAM = 'sort.ty'
 
 def main():
-    filename = os.path.join(PROGRAMS_DIR, FILENAME)
-    file = open(filename)
+    # Check por program argument
+    filename = os.path.join(PROGRAMS_DIR, DEFAULT_PROGRAM)
+    if (len(sys.argv)) > 1:
+        if sys.argv[1] != '-debug':
+            filename = os.path.join(PROGRAMS_DIR, sys.argv[1])
+
+    # Open file with error handling
+    try:
+        file = open(filename, 'r')
+    except OSError:
+        print ("Could not find file at: ", filename)
+        sys.exit()
+
+
     data = file.read()
     file.close()
 
+    # Run Compiler
     compiler = Compiler()
-    json_data = compiler.compile(data, debug=True)
+    
+    # Check -debug flag
+    is_debug = False
+    if (len(sys.argv) == 2):
+        is_debug = sys.argv[1] == '-debug'
+    elif (len(sys.argv) > 2):
+        is_debug = sys.argv[2] == '-debug'
 
+    json_data = compiler.compile(data, debug=is_debug)
+
+    # Run Virtual Machine
     virtual_machine = VirtualMachine()
     virtual_machine.run(json_data)
 
